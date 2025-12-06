4586210375dbe933b184a1fa44faf85ec7ed0211
Erik Padrón <epadron94@icloud.com>
2025-12-05 23:18:00 -0600

    show error banner for error messages

---
+++ MvcMovie/Models/Static.cs
@@ -0,0 +1,15 @@
+
+namespace MvcMovie.Static
+{
+    public static class Static
+    {
+        public static string KeyVault = "KeyVault";
+        public static string VaultUri = "VaultUri";
+        public static string CosmosDbAccount = "CosmosDbAccount";
+        public static string CosmosDbKey = "CosmosDbKey";
+        public static string MvcMovieContext = "MvcMovieContext";
+        public static string CosmosDb = "CosmosDb";
+        public static string PreferredRegions = "PreferredRegions";
+    }
+
+}
